22c8377e772495e094af15bebb2c90dd07fb7bc3
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2023-10-07 13:58:04 +0545

    user route create

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -3,8 +3,14 @@ import * as userController from 'controller/user.controller';
 
 const route = Router();
 
+route.get('/', userController.getAllUsers);
+
 route.post('/', userController.createUser);
 
 route.get('/:id', userController.getUser);
+
+route.patch('/:id', userController.updateUser);
+
+route.delete('/:id', userController.deleteUser);
 
 export default route;
