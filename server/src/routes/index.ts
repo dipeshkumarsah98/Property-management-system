9ac0c9dcfd6da43add38f971befb5e02edd31bd8
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2023-10-07 13:59:06 +0545

    index route created for all routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userRouter from 'routes/user.route';
+import roleRouter from 'routes/role.route';
 
 const apiRouter = Router();
 
 apiRouter.use('/users', userRouter);
+apiRouter.use('/roles', roleRouter);
 
 export default apiRouter;
