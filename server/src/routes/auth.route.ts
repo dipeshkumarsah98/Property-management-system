171f8bbaa6d245868bc9fc1e3ddfaa391d2dbb0d
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2023-10-31 09:47:55 +0545

    new route add to send otp

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import * as authController from 'controller/auth.controller';
+import { verifyToken } from 'middleware/auth';
 
 const route = Router();
 
+route.post('/send-otp', authController.sendOtp);
+
 route.post('/login', authController.loginUser);
 
-route.post('/register', authController.registerUser);
+route.post('/register', verifyToken, authController.registerUser);
 
 route.post('/refresh', authController.refreshUserToken);
 
