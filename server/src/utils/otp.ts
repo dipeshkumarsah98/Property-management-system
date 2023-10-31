89022c15a1ea8b815d49c8d5907afa67f73bdc2b
Dipesh Kumar Sah <dipeshsah98@gmail.com>
2023-10-31 09:46:04 +0545

    add expire time to opt

--- server/src/utils/otp.ts
+++ server/src/utils/otp.ts
@@ -1,7 +1,11 @@
 import env from 'config/env.config';
 import { totp } from 'otplib';
 
-const { OTP_SECRET } = env;
+const { OTP_SECRET, OTP_DURATION_IN_SECS } = env;
+console.log('🚀 ~ file: otp.ts:5 ~ OTP_SECRET:', OTP_SECRET);
+totp.options = {
+  step: +OTP_DURATION_IN_SECS,
+};
 
 const generateOtp = () => totp.generate(OTP_SECRET);
 
